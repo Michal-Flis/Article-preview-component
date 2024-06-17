fa91aa2fbcd1d4d8da6d320b6adf67a98c0e9686
Michał Flis <86780656+Michal-Flis@users.noreply.github.com>
2024-06-17 17:04:02 +0200

    Desktop

--- script.js
+++ script.js
@@ -4,15 +4,31 @@ const share = document.querySelector('.share');
 const activeShare = document.querySelector('.active-share');
 
 btn.addEventListener('click', () => {
-  share.classList.toggle('hide');
-  activeShare.classList.toggle('hide');
+  if (screen.width >= 320 && screen.width < 1024) {
+    share.classList.toggle('hide');
+    activeShare.classList.toggle('hide');
 
-  activeShare.style.animation = 'entry .5s 1 linear';
+    if (screen.width === 320) {
+      activeShare.style.animation = 'entry320 .5s 1 linear';
+    } else if (screen.width === 375) {
+      activeShare.style.animation = 'entry375 .5s 1 linear';
+    } else if (screen.width === 768) {
+      activeShare.style.animation = 'entry768 .5s 1 linear';
+    }
+  } else if (screen.width >= 1024) {
+    console.log('działa');
+  }
 });
 
 activeBtn.addEventListener('click', () => {
   share.classList.toggle('hide');
   activeShare.classList.toggle('hide');
 
-  share.style.animation = 'entry .5s linear';
+  if (screen.width === 320) {
+    share.style.animation = 'entry320 .5s linear';
+  } else if (screen.width === 375) {
+    share.style.animation = 'entry375 .5s linear';
+  } else if (screen.width) {
+    share.style.animation = 'entry .5s linear';
+  }
 });
