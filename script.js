b5db71e73a9442d0bafd10c1fa4d7f2e392bc676
Michał Flis <86780656+Michal-Flis@users.noreply.github.com>
2024-06-19 14:10:13 +0200

    Modified all code and finish project

--- script.js
+++ script.js
@@ -10,15 +10,7 @@ btn.addEventListener('click', () => {
     share.classList.toggle('hide');
     activeShare.classList.toggle('hide');
 
-    if (screen.width === 320) {
-      activeShare.style.animation = 'entry320 .5s 1 linear';
-    } else if (screen.width === 375) {
-      activeShare.style.animation = 'entry375 .5s 1 linear';
-    } else if (screen.width === 425) {
-      activeShare.style.animation = 'entry375 .5s 1 linear';
-    } else if (screen.width === 768) {
-      activeShare.style.animation = 'entry768 .5s 1 linear';
-    }
+    activeShare.style.animation = 'entry 0.5s 1 linear';
   } else if (screen.width >= 1024) {
     activeDesktop.classList.toggle('active-desktop-btn');
     tail.classList.toggle('active-desktop-btn');
@@ -29,13 +21,5 @@ activeBtn.addEventListener('click', () => {
   share.classList.toggle('hide');
   activeShare.classList.toggle('hide');
 
-  if (screen.width === 320) {
-    share.style.animation = 'entry320 .5s linear';
-  } else if (screen.width === 375) {
-    share.style.animation = 'entry375 .5s linear';
-  } else if (screen.width === 425) {
-    share.style.animation = 'entry375 .5s linear';
-  } else if (screen.width === 768) {
-    share.style.animation = 'entry768 .5s linear';
-  }
+  share.style.animation = 'entry 0.5s linear';
 });
